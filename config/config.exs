90a88135ff8c0f3902d60d1849d1f58078330a1c
Kevin Asbury <kasbury@pursuitky.com>
2019-03-04 20:55:09 -0500

    ueberauth config

--- config/config.exs
+++ config/config.exs
@@ -1,8 +1,3 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
 use Mix.Config
 
 # General application configuration
@@ -22,6 +17,10 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
+config :ueberauth, Ueberauth,
+  providers: [
+    github: {Ueberauth.Strategy.Github, []}
+  ]
+
+import_config "config.secret.exs"
 import_config "#{Mix.env}.exs"
--- config/dev.exs
+++ config/dev.exs
@@ -1,11 +1,6 @@
 use Mix.Config
 
-# For development, we disable any cache and enable
-# debugging and code reloading.
-#
-# The watchers configuration can be used to run external
-# watchers to your application. For example, we use it
-# with brunch.io to recompile .js and .css sources.
+
 config :discuss, Discuss.Endpoint,
   http: [port: 4000],
   debug_errors: true,
@@ -33,11 +28,4 @@ config :logger, :console, format: "[$level] $message\n"
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
 
-# Configure your database
-config :discuss, Discuss.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "discuss_dev",
-  hostname: "localhost",
-  pool_size: 10
+import_config "dev.secret.exs"
