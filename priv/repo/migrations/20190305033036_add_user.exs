6ce25a231476649014ae44cecdc53504f0987cf8
Kevin Asbury <kasbury@pursuitky.com>
2019-03-04 22:47:55 -0500

    creating new users

---
+++ priv/repo/migrations/20190305033036_add_user.exs
@@ -0,0 +1,13 @@
+defmodule Discuss.Repo.Migrations.Add_User do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :email, :string
+      add :provider, :string
+      add :token, :string
+
+      timestamps()
+    end
+  end
+end
